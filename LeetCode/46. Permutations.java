969331e8110ac6289bfd523650aaba71fc1e35e5
Shashank Hariharan <Shashumj05@gmail.com>
2026-07-26 22:01:16 +0530

    Day 13

---
+++ LeetCode/46. Permutations.java
@@ -0,0 +1,38 @@
+class Solution {
+
+    List<List<Integer>> result = new ArrayList<>();
+
+    public List<List<Integer>> permute(int[] nums) {
+
+        boolean[] used = new boolean[nums.length];
+
+        backtrack(nums, used, new ArrayList<>());
+
+        return result;
+    }
+
+    private void backtrack(int[] nums,
+                           boolean[] used,
+                           List<Integer> current) {
+
+        if (current.size() == nums.length) {
+
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (used[i])
+                continue;
+
+            used[i] = true;
+            current.add(nums[i]);
+
+            backtrack(nums, used, current);
+
+            current.remove(current.size() - 1);
+            used[i] = false;
+        }
+    }
+}
