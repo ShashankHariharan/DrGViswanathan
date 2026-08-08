68dfa86aa6aafb2e8567720a34ead0cfeb5c5b1a
Shashank Hariharan <Shashumj05@gmail.com>
2026-08-08 20:50:35 +0530

    Day 21

---
+++ LeetCode/144. Binary Tree Preorder Traversal.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+
+        List<Integer> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+
+            TreeNode node = stack.pop();
+
+            // Root
+            result.add(node.val);
+
+            // Right first
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+
+            // Left second
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+
+        return result;
+    }
+}
