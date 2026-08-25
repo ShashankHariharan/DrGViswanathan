0efb094411cdded769843637c21701ad735a0da5
Shashank Hariharan <Shashumj05@gmail.com>
2026-08-25 21:43:56 +0530

    Day 30

---
+++ LeetCode/71. Simplify Path.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Deque<String> stack = new ArrayDeque<>();
+
+        for (String part : path.split("/")) {
+            if (part.isEmpty() || part.equals(".")) {
+                continue;
+            }
+
+            if (part.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(part);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        while (!stack.isEmpty()) {
+            result.append('/').append(stack.removeLast());
+        }
+
+        return result.length() == 0 ? "/" : result.toString();
+    }
+}
